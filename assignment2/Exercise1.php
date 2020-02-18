e5dd04f5280b022169ec4f69886a02c1fe2cd752
Bddouglas <bddouglas@wccnet.edu>
2020-02-17 20:06:34 -0500

    update

--- assignment2/Exercise1.php
+++ assignment2/Exercise1.php
@@ -27,7 +27,7 @@ echo '\n ';
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>&lt;inject title here&gt;</title>
+	<title>&lt;nested list&gt;</title>
 	<style>
 		* {margin: 0; padding: 0;}
 		body {font: 120%/1.5 sans-serif;}
