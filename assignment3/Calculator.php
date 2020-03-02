4dade5275e527db6e581ca873981c6eff244ee96
Bddouglas <bddouglas@wccnet.edu>
2020-03-02 16:04:23 -0500

    Update

--- assignment3/Calculator.php
+++ assignment3/Calculator.php
@@ -11,35 +11,34 @@ public $operator;
 
 
 
-public function __construct( $operator, $num1, $num2 ) {
-    $this-> $operator = 'error';
-     $this-> $num1 = 'error';
-     $this-> $num2 = 'error';
-    }
+//public function __construct($operator="error", $num1="error", $num2="error"){
 
-public function Calc($operator, $num1,$num2){
+
+//}
+
+public function Calc($operator="error", $num1="error",$num2="error"){
    
     
     
        
     
-    if($num1 !=int or $num2 !=int){
-        echo "Must enter a String and two numbers";
-    }else if($operator != '+' or $operator != '-' or $operator != '*' or $operator != '/'){
-echo "Invalid operator";
+    if($operator != '+' and $operator != '-' and $operator != '*' and $operator != '/'){
+        echo "Invalid operator";
+    }else if(!is_int($num1) || !is_int($num2)){
+return "Must enter string and two numbers";
     }else {
-    if ($operator ==="+"){
+    if ($operator =="+"){
         
         return $num1 + $num2;
         }
-        else if ($operator ==="-"){ 
+        else if ($operator =="-"){ 
          return $num1 - $num2;
         }
-        else if ($operator ==="*"){
+        else if ($operator =="*"){
         return $num1 * $num2;
         }
         
-        else if($operator== "/"){
+        else if($operator == "/"){
             if($num2 == 0){
                 echo "Can not divide by 0";
             }else{
@@ -49,7 +48,7 @@ echo "Invalid operator";
 
         }
 
-    }
+    }//end of if statement
 
 }//end of function
 
